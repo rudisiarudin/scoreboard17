0b7cc99b39fe82a33996f4ba786fdad8c7ff9bb3
rudisiarudin <80684819+rudisiarudin@users.noreply.github.com>
2025-08-13 13:08:28 +0700

    Update seed.ts

--- client/src/lib/seed.ts
+++ client/src/lib/seed.ts
@@ -5,7 +5,7 @@ export const initialState: BoardState = {
   teams: [
     {
       id: "kel1",
-      name: "Merdeka",
+      name: "KELOMPOK 1",
       color: "#EF4444",
       members: [
         "Andrias","Asmawati","Desi","Felly","Intan","Katherine","Pipin","Rayviansyah","Ruby","Tunggul","Widya"
@@ -13,7 +13,7 @@ export const initialState: BoardState = {
     },
     {
       id: "kel2",
-      name: "Bersatu",
+      name: "KELOMPOK 12",
       color: "#10B981",
       members: [
         "Asep","Irsan","Lisi","Neysa","Ramdoni","Ryandhi","Sariana","Thomas","Yohan"
@@ -21,7 +21,7 @@ export const initialState: BoardState = {
     },
     {
       id: "kel3",
-      name: "Berjuang",
+      name: "KELOMPOK 3",
       color: "#F97316",
       members: [
         "Coraevi","Dinny","Harvey","Hilaluddin","Juni","Kevin","Lydia","Petrus","Stephanie","Yunus"
@@ -29,7 +29,7 @@ export const initialState: BoardState = {
     },
     {
       id: "kel4",
-      name: "Bangkit",
+      name: "KELOMPOK 4",
       color: "#3B82F6",
       members: [
         "Aldri","Dwi","Eliaanti","Mian","Nancy","Nike","Said","Sarwono","Stefanini","Titis"
@@ -37,14 +37,14 @@ export const initialState: BoardState = {
     },
     {
       id: "kel5",
-      name: "Berdaulat",
+      name: "KELOMPOK 5",
       color: "#A855F7",
       members: [
         "Andri","Donny","Hadly","Merly","Novitasari","Parawinata","Rahmat","Suryadi","Susilo","Yudha","Yuni"
       ],
     },
     {
-      id: "kel6",
+      id: "KELOMPOK 6",
       name: "Indonesia Jaya",
       color: "#F59E0B",
       members: [
@@ -59,7 +59,7 @@ events: [
   { id: "ev-tebak-lagu", name: "Lomba Tebak Lagu Nasional", weight: 100 },
   { id: "ev-estafet-balon", name: "Lomba Estafet Balon", weight: 100 },
   { id: "ev-best-costume", name: "Best Costume", weight: 100 },
-  { id: "ev-potluck", name: "Potluck Merah Putih – “Cita Rasa Nusantara”", weight: 100 },
+  { id: "ev-potluck", name: "Potluck Merah Putih", weight: 100 },
 ],
   scores: {},
 };
